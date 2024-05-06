505340c1a73e2c46328d646a7689712267142b54
kavi castelo <kavindu123450@gmail.com>
2024-05-07 00:46:18 +0530

    patch-environments
    set live environments

--- src/main/java/com/hris/HRIS/CorsConfig.java
+++ src/main/java/com/hris/HRIS/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1") // Specify the mapping pattern you want to allow CORS for
-                .allowedOrigins("http://localhost:4200") // Allow requests from this origin uncomment this line to run on localhost
+                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://hris-v1.netlify.app/") // Allow requests from this origin uncomment this line to run on localhost
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow sending credentials (cookies, authentication headers)
@@ -26,6 +27,7 @@ public class CorsConfig implements WebMvcConfigurer {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:4200"); // uncomment this line to run on localhost
+        config.addAllowedOrigin("https://hris-v1.netlify.app/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
